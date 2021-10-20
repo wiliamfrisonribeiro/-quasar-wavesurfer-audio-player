8ffe4e965a0ba2b7c222a50777cb655869471774
Wiliam Ribeiro <wiliam@webcgi.com.br>
2021-10-20 09:20:34 -0300

    adding controls

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="waveform"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import WaveSurfer from "wavesurfer.js";
+export default defineComponent({
+  name: "PageIndex",
+
+  data: () => ({
+    wavesurfer: null,
+  }),
+
+  async mounted() {
+    if (!this.wavesurfer) this.createWaveSurfer();
+  },
+  methods: {
+    createWaveSurfer() {
+      this.wavesurfer = WaveSurfer.create({
+        container: "#waveform",
+        barWidth: 3,
+      });
+
+      this.wavesurfer.load(
+        "https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+      );
+    },
+  },
+});
+</script>
